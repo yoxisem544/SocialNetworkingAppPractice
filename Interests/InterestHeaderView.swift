7d040b7f6119d463b633e2b1301c4d82ace6d18d
David <yoxisem544@gmail.com>
2015-08-25 16:42:46 +0800

    check point 2

---
+++ Interests/InterestHeaderView.swift
@@ -0,0 +1,42 @@
+//
+//  InterestHeaderView.swift
+//  Interests
+//
+//  Created by David on 2015/8/25.
+//  Copyright © 2015年 David. All rights reserved.
+//
+
+import UIKit
+
+class InterestHeaderView: UIView {
+    
+    // MARK: - public api
+    var interest: Interest! {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        backgroundImageView?.image! = interest.featuredImage
+        interestTitleLabel?.text! = interest.title
+        numberOfMembersLabel?.text! = "\(interest.numberOfMembers)"
+        numberOfPostsLabel?.text! = "\(interest.numberOfPosts)"
+        pullDownToCloseLabel?.text! = "Pull down to close"
+        pullDownToCloseLabel?.hidden = true
+    }
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var interestTitleLabel: UILabel!
+    @IBOutlet weak var numberOfMembersLabel: UILabel!
+    @IBOutlet weak var numberOfPostsLabel: UILabel!
+    @IBOutlet weak var pullDownToCloseLabel: UILabel!
+    @IBOutlet weak var closeButtonBackgroundView: UIView!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        closeButtonBackgroundView.layer.cornerRadius = closeButtonBackgroundView.bounds.width / 2
+    }
+
+}
